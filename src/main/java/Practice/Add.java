156ba3b3ed386f2dabc39f835bde9c2466d8a432
HashGowda <harishsr594@gmail.com>
2023-02-02 11:17:19 +0530

    Addition code added

---
+++ src/main/java/Practice/Add.java
@@ -0,0 +1,14 @@
+package Practice;
+
+public class Add {
+
+    void addition(int a, int b){
+        int sum=a+b;
+        System.out.println(sum);
+    }
+
+    public static void main(String[] args) {
+        Add ad = new Add();
+        ad.addition(2,4);
+    }
+}
